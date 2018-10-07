27a199e479d9aabdeae82a5bb3439e7a50477073
Jacy <1399739901@qq.com>
2018-10-07 17:28:33 +0800

    Jacy 2018/10/07

--- src/main/java/com/nchhr/platform/PlatformApplication.java
+++ src/main/java/com/nchhr/platform/PlatformApplication.java
@@ -1,5 +1,6 @@
 package com.nchhr.platform;
 
+import com.nchhr.platform.util.WeChatUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,5 +11,7 @@ public class PlatformApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlatformApplication.class, args);
+
+//        WeChatUtil.CCSR();
     }
 }
