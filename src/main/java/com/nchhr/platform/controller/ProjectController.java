28aa63ab6fc9186284ec30ae1096b05cf9ec2646
Jacy <1399739901@qq.com>
2018-10-07 11:36:58 +0800

    Jacy 2018/10/07 修改项目展示控制类

--- src/main/java/com/nchhr/platform/controller/ProjectController.java
+++ src/main/java/com/nchhr/platform/controller/ProjectController.java
@@ -4,8 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/project")
 //平台项目相关控制器层，展示我的项目和项目详情
 public class ProjectController {
 
+    //我的项目
+    @RequestMapping("/my")
+    public String myProject() {
+
+        return "myProject";
+    }
+
+    //项目详情
+    @RequestMapping("/detail")
+    public String projectDetail() {
+
+        return "projectDetail";
+    }
 }
