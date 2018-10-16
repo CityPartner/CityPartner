114d7605cd79e674730cc782e1528f9ac77ed950
Jacy <1399739901@qq.com>
2018-10-16 17:11:05 +0800

    JC 2018/10/13 微信接口配置

--- src/main/java/com/nchhr/platform/PlatformApplication.java
+++ src/main/java/com/nchhr/platform/PlatformApplication.java
@@ -4,14 +4,24 @@ import com.nchhr.platform.util.WeChatUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @MapperScan("com.nchhr.platform.dao")
-public class PlatformApplication {
+public class PlatformApplication extends SpringBootServletInitializer implements WebApplicationInitializer
+{
 
     public static void main(String[] args) {
         SpringApplication.run(PlatformApplication.class, args);
 
-//        WeChatUtil.CCSR();
+        WeChatUtil.CCSR();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+        return builder.sources(PlatformApplication.class);
     }
 }
