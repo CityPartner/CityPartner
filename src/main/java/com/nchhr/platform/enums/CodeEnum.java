0357b39dd44dec62a887de9b64973745a4c50347
DDLD <747413073@qq.com>
2018-11-04 15:39:49 +0800

    register

--- src/main/java/com/nchhr/platform/enums/CodeEnum.java
+++ src/main/java/com/nchhr/platform/enums/CodeEnum.java
@@ -6,7 +6,20 @@ public enum CodeEnum {
     AccessKeySecre("AccessKeySecret", "nDr1n0ZRemha5MrkYnQ2ZLXprTmTdM"),
     //短信模板
     SMSTemplateCode("SMSTemplateCode", "SMS_146807344"),
-    SignName("SignName", "南昌城市合伙人"),;
+    SignName("SignName", "南昌城市合伙人"),
+
+    //短信提醒提现
+    SMSTemplateCode1("SMSTemplateCode","SMS_150172325"),
+    SignName1("SignName", "平台提醒"),
+
+
+    //短信申请验证码
+    SMSTemplateCode2("SMSTemplateCode","SMS_150182420"),
+    SignName2("SignName", "申请提现"),;
+
+
+
+
 
     private String key;
     private String value;
--- src/main/java/com/nchhr/platform/util/SmsUtils.java
+++ src/main/java/com/nchhr/platform/util/SmsUtils.java
@@ -8,74 +8,26 @@ import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
-import com.nchhr.platform.entity.PhoneCodeEntity;
 import com.nchhr.platform.enums.CodeEnum;
 
 import javax.servlet.http.HttpSession;
 
-/**
- * 获取验证码
- * 1、发送验证码成功
- * 4、验证码获取频繁
- * 3、发送失败
- */
-
-
-public class GetCodeUtils {
-
-    /**
-     * @param phone   手机
-     * @param session
-     * @param model   //使用模板
-     * @return
-     */
-    public static String getCode(String phone, HttpSession session, String model) {
-
+public class SmsUtils {
+    public static String send(String phone, String name, HttpSession session, String model) {
 
         CodeEnum codeEnumAccessId = CodeEnum.AccessId;
         CodeEnum codeEnumAccessKeySecre = CodeEnum.AccessKeySecre;
         CodeEnum codeEnumSignName = CodeEnum.SignName;
         CodeEnum codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode;
-
         if (model == "1") {
-            //注册
-            codeEnumAccessId = CodeEnum.AccessId;
-            codeEnumAccessKeySecre = CodeEnum.AccessKeySecre;
-            codeEnumSignName = CodeEnum.SignName;
-            codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode;
-        } else if (model == "2") {
-            //商城找回密码
-            codeEnumAccessId = CodeEnum.AccessId;
-            codeEnumAccessKeySecre = CodeEnum.AccessKeySecre;
-            codeEnumSignName = CodeEnum.SignName;
-            codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode;
-        } else if (model == "3") {
-            //平台注册
-            codeEnumAccessId = CodeEnum.AccessId;
-            codeEnumAccessKeySecre = CodeEnum.AccessKeySecre;
-            codeEnumSignName = CodeEnum.SignName;
-            codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode;
-        } else if (model == "4") {
-            //平台找回密码
-            codeEnumAccessId = CodeEnum.AccessId;
-            codeEnumAccessKeySecre = CodeEnum.AccessKeySecre;
-            codeEnumSignName = CodeEnum.SignName;
-            codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode;
-        } else if (model == "5") {
-            //申请提现
-            codeEnumAccessId = CodeEnum.AccessId;
-            codeEnumAccessKeySecre = CodeEnum.AccessKeySecre;
-            codeEnumSignName = CodeEnum.SignName2;
-            codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode2;
+
         } else {
-            //默认
+            //短信提醒
             codeEnumAccessId = CodeEnum.AccessId;
             codeEnumAccessKeySecre = CodeEnum.AccessKeySecre;
-            codeEnumSignName = CodeEnum.SignName;
-            codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode;
+            codeEnumSignName = CodeEnum.SignName1;
+            codeEnumSMSTemplateCode = CodeEnum.SMSTemplateCode1;
         }
-
-
         //获取随机验证码，保存在session中
         CodeUtils codeUtils = new CodeUtils();
         //设置超时时间-可自行调整
@@ -87,13 +39,13 @@ public class GetCodeUtils {
         //替换成你的AK
         final String accessKeyId = codeEnumAccessId.getValue();//你的accessKeyId,参考本文档步骤2
         final String accessKeySecret = codeEnumAccessKeySecre.getValue();//你的accessKeySecret，参考本文档步骤2
-        //获得随机验证码
-        String usercode = codeUtils.createRandom(true, 6);
-        //保存phone code到session中
-        if (GetCodeUtils.save(phone, usercode, session) == false) {
-            //4代表获取验证码频繁
-            return "4";
-        }
+//        //获得随机验证码
+//        String usercode = codeUtils.createRandom(true, 6);
+//        //保存phone code到session中
+//        if (GetCodeUtils.save(phone, usercode, session) == false) {
+//            //4代表获取验证码频繁
+//            return "4";
+//        }
         //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
@@ -115,7 +67,7 @@ public class GetCodeUtils {
         request.setTemplateCode(codeEnumSMSTemplateCode.getValue());
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-        request.setTemplateParam("{\"code\":\"" + usercode + "\"}");
+        request.setTemplateParam("{\"name\":\"" + name + "\"}");
         //可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
         //request.setSmsUpExtendCode("90997");
         //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
@@ -138,58 +90,4 @@ public class GetCodeUtils {
 
         return "3";
     }
-
-    //保存验证码
-    public static boolean save(String phone, String code, HttpSession session) {
-
-        if (session.getAttribute(phone) != null) {
-            return false;
-        }
-        try {
-            PhoneCodeEntity phoneCode = new PhoneCodeEntity();
-            MD5Utils md5Utils = new MD5Utils();
-            phoneCode.setPhone(phone);
-            phoneCode.setCode(md5Utils.MD5Encode(code, "utf8"));
-
-            session.setAttribute(phone, phoneCode);
-        } catch (Exception e) {
-
-            return false;
-        }
-        return true;
-    }
-
-
-    //判断验证码
-
-    /**
-     * @param userPhone
-     * @param code
-     * @param session
-     * @return "5"验证码过期 ， ”4“系统异常, "1"验证码成功，”2“验证码错误
-     */
-    public static String judgeCode(String userPhone, String code, HttpSession session) {
-
-        MD5Utils md5Utils = new MD5Utils();
-        PhoneCodeEntity phoneCode = (PhoneCodeEntity) session.getAttribute(userPhone);
-        if (phoneCode == null) {
-            return "5";
-        } else {
-            String mdCode;
-            try {
-                mdCode = md5Utils.MD5Encode(code, "utf8");
-
-            } catch (Exception e) {
-                return "4";
-            }
-            if (mdCode.equals(phoneCode.getCode())) {
-                return "1";
-            } else {
-                //验证码错误
-                return "2";
-            }
-        }
-    }
-
-
 }
